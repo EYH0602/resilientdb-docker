57f81eb845541d9aef88ce343dba8123d8c8d45d
Yifeng He <yfhe.prsn@gmail.com>
2024-12-06 22:47:16 -0800

    update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,3 @@ cd ..
 # start graph ql
 cd resilientdb-graphql
 nohup bazel-bin/service/http_server/crow_service_main service/tools/config/interface/service.config service/http_server/server_config.config
-python3 app.py
